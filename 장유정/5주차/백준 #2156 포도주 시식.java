427fb7ac5d280b3a05551304c34e3a2dcc0ed9bb
You Jung <80906691+JangYouJung@users.noreply.github.com>
2024-05-18 23:56:49 +0900

    [장유정] 5주차 개인과제[1]

--- 장유정/5주차/백준 #2156 포도주 시식.java
+++ 장유정/5주차/백준 #2156 포도주 시식.java
@@ -23,9 +23,7 @@ public class Main {
         if( N > 1 ) dp[1] = wine[0] + wine[1];
         if( N > 2 ) dp[2] = Math.max( dp[1], Math.max( wine[0] + wine[2], wine[1] + wine[2]));
 
-        for( int i = 0; i < N; i++ ){
-            
-            
+        for( int i = 3; i < N; i++ ){
             dp[i] = Math.max( dp[i-1], Math.max( dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i] ));
         }
 
